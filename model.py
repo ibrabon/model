7691eb0037b1df8b2a5e495e2e2f1dc82fa1a4ba
ibrabon <antilooser@inbox.ru>
2013-07-09 09:34:44 +0400

    Reading parametrs from objects

--- model.py
+++ model.py
@@ -13,7 +13,22 @@ class ModelFrame(wx.Frame):
 		self.label1 = wx.StaticText(panel, -1, "Solution:",pos=(20,200))
 		self.label2= wx.StaticText(panel,-1,"",pos=(70,200))
 		self.slider.SetTickFreq(5)
+		self.Bind(wx.EVT_BUTTON,self.OnCloseMe, self.button)
+		self.Bind(wx.EVT_CLOSE,self.OnCloseWindow)
+		self.Bind(wx.EVT_BUTTON,self.Solving,self.button1)
+		
+		
+		
+	def OnCloseMe(self,event):
+		self.Close(True)
 	
+	def Solving(self,event):
+		s = self.slider.Value
+		self.label2.SetLabelText(str(s))
+	
+	def OnCloseWindow(self,event):
+		self.Destroy()
+		
 if __name__ == '__main__':
 	app=wx.PySimpleApp()
 	frame=ModelFrame()
